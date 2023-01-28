9973da6da59267b6431187d165a55c3dac55c1cd
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-27 23:16:08 -0800

    add some styling

--- controllers/compose.js
+++ controllers/compose.js
@@ -1,115 +1,107 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../models')
-const axios = require('axios')
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const axios = require("axios");
 
 //pulling data from quotable api
-router.get('/new', (req, res) => {
-    axios.get(`https://api.quotable.io/random?maxLength=80#`)
-        .then(response => {
-            res.render('compose.ejs', { quote: response.data })
-        })
-        .catch(console.log())
-})
+router.get("/new", (req, res) => {
+  axios
+    .get(`https://api.quotable.io/random?maxLength=80#`)
+    .then((response) => {
+      res.render("compose.ejs", { quote: response.data });
+    })
+    .catch(console.log());
+});
 //showing single compose/post to the showcompose.ejs page
-router.get('/:id', async (req, res) => {
-    try {
-        //find specific post from db -- with join sql request
-        const compose = await db.compose.findByPk(req.params.id, { include: [db.comment] })
-        res.render('showcompose.ejs', { compose })
-
-    } catch (err) {
-        console.log(err)
-    }
-})
-
+router.get("/:id", async (req, res) => {
+  try {
+    //find specific post from db -- with join sql request
+    const compose = await db.compose.findByPk(req.params.id, {
+      include: [db.comment],
+    });
+    res.render("showcompose.ejs", { compose });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //creates an compose/post with the form data - when user click publish button
 router.post("/", async (req, res) => {
-    //create compose object with columns
-    const compose = await db.compose.create({
-        quote: req.body.quote,
-        author: req.body.author,
-        body: req.body.postBody,
-        userId: res.locals.user.dataValues.id
-
-    })
-    //redirect the user once the post has been create in db
-    res.redirect(`compose/${compose.id}`)
-
+  //create compose object with columns
+  const compose = await db.compose.create({
+    quote: req.body.quote,
+    author: req.body.author,
+    body: req.body.postBody,
+    userId: res.locals.user.dataValues.id,
+  });
+  //redirect the user once the post has been create in db
+  res.redirect(`compose/${compose.id}`);
 });
 
-//create route that add comments to compose/post 
+//create route that add comments to compose/post
 //url ---> /compose/:id/comment
 router.post("/:id/comment", async (req, res) => {
-    try {
-
-        //create comment for the compuseQuote in db
-        const newComment = await db.comment.create({
-
-            body: req.body.body,
-            userId: res.locals.user.id,
-            composeId: req.params.id
-            
-
-        })
-        console.log(newComment)
-        //redirect shows compose and new comment
-        res.redirect(`/compose/${req.params.id}`)
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    //create comment for the compuseQuote in db
+    const newComment = await db.comment.create({
+      body: req.body.body,
+      userId: res.locals.user.id,
+      composeId: req.params.id,
+    });
+    console.log(newComment);
+    //redirect shows compose and new comment
+    res.redirect(`/compose/${req.params.id}`);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //shows the edit form to the user
 router.get("/:id/edit", async (req, res) => {
-    try {
-        let quote = await db.compose.findByPk(req.params.id)
-        if (quote.userId !== res.locals.user.dataValues.id) {
-            res.render('notauthorized.ejs')
-            return
-        }
-        res.render("edit", { quote });
-
-    } catch (err) {
-        console.log(err)
+  try {
+    let quote = await db.compose.findByPk(req.params.id);
+    if (quote.userId !== res.locals.user.dataValues.id) {
+      res.render("notauthorized.ejs");
+      return;
     }
-})
+    res.render("edit", { quote });
+  } catch (err) {
+    console.log(err);
+  }
+});
 //update data for specific id of compose
-router.put('/:id', async (req, res) => {
-    try {
-        let composeBody = await db.compose.findByPk(req.params.id)
-        console.log(composeBody)
-        if (composeBody.userId !== res.locals.user.dataValues.id) {
-            res.render('notauthorized.ejs')
-            return
-        }
-        composeBody.body = req.body.postBody
-        composeBody.quote = req.body.quote
-        composeBody.author = req.body.author
-        composeBody.save()
-
-        res.redirect('/');
-    } catch (err) {
-        console.log(err)
+router.put("/:id", async (req, res) => {
+  try {
+    let composeBody = await db.compose.findByPk(req.params.id);
+    console.log(composeBody);
+    if (composeBody.userId !== res.locals.user.dataValues.id) {
+      res.render("notauthorized.ejs");
+      return;
     }
+    composeBody.body = req.body.postBody;
+    composeBody.quote = req.body.quote;
+    composeBody.author = req.body.author;
+    composeBody.save();
+
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //delete specific posts from db
 router.delete("/:id", async (req, res) => {
-    try {
-        let quote = await db.compose.findByPk(req.params.id)
-        if (quote.userId !== res.locals.user.dataValues.id) {
-            res.render('notauthorized.ejs')
-            return
-        }
-        await quote.destroy()
-        res.redirect("/");
-
-    } catch (err) {
-        console.log(err)
+  try {
+    let quote = await db.compose.findByPk(req.params.id);
+    if (quote.userId !== res.locals.user.dataValues.id) {
+      res.render("notauthorized.ejs");
+      return;
     }
-})
+    await quote.destroy();
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
